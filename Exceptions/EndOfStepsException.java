579c13bc30db3f84cd646d6c21b613b3e71d3533
jasminaorlowska <jorlowska3@st.swps.edu.pl>
2023-10-22 16:11:41 +0200

    changed names of folders

--- Exceptions/EndOfStepsException.java
+++
@@ -1,8 +0,0 @@
-package Exceptions;
-/**End of steps exceptions - while debugging*/
-public class EndOfStepsException extends Exception{
-    public EndOfStepsException(String message) {
-        super("Next instruction: " + message);
-    };
-
-}
--- Exceptions/UndefinedVariableException.java
+++
@@ -1,7 +0,0 @@
-package Exceptions;
-
-public class UndefinedVariableException extends Exception{
-    public UndefinedVariableException(char c) {
-        super("MacchiatoInstructions.Variable " + c + " is undefined.");
-    }
-}
